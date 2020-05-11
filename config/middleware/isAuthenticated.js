280c92cc58548839310f974d36782ceeef5bbe80
David Hawkins <uxhawk@gmail.com>
2020-05-10 20:23:26 -0400

    switched to single quotes around variables and removed padding

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,4 +1,5 @@
-// This is middleware for restricting routes a user is not allowed to visit if not logged in
+// This is middleware for restricting routes a user is
+// not allowed to visit if not logged in
 module.exports = function(req, res, next) {
   // If the user is logged in, continue with the request to the restricted route
   if (req.user) {
@@ -6,5 +7,5 @@ module.exports = function(req, res, next) {
   }
 
   // If the user isn't logged in, redirect them to the login page
-  return res.redirect("/");
+  return res.redirect('/');
 };
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   $('.second-button').on('click', function() {
     $('.animated-icon2').toggleClass('open');
   });
