8c2f87f6292af5d1a74938557d80dc7f6a9c9fb7
David Hawkins <uxhawk@gmail.com>
2020-05-16 11:35:35 -0400

    config updates for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
+/* eslint-disable no-unused-vars */
 // *****************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *************************************************
 
 // Dependencies
 const Sequelize = require('sequelize');
+const mysql = require('mysql');
 
 // Creates mySQL connection using Sequelize,
 // the empty string in the third argument spot is our password.
-const sequelize = new Sequelize('sequelize_library', 'root', '', {
-  host: 'localhost',
-  port: 3306,
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  const sequelize = new Sequelize('sequelize_library', 'root', '', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+  });
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
