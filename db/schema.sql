b007b77c08ffb48adf8802002ff7f2d3a83fa27d
hafiz salman <hmsalmans1@gmail.com>
2020-05-18 00:18:52 +0500

    more codes for languages

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
-CREATE DATABASE vocabulary_db;
+USE vocabulary_db;
+
+
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Dutch', 'nl', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Urdu', 'ur', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Terkish', 'tr', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Russian', 'ru', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Italian', 'it', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Hebrew', 'he', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Greek', 'el', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Arabic', 'ar', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Chinese', 'zh', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('French', 'fr', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Hindi', 'hi', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Hungarian', 'hu', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Japanese', 'ja', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Korean', 'ko', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Polish', 'pl', '2020-05-17', '2020-05-17');
+INSERT INTO `vocabulary_db`.`language` (`lang`, `lang_code`, `createdAt`, `updatedAt`) VALUES ('Swedish', 'sv', '2020-05-17', '2020-05-17');
